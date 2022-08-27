93cde60678e41b5548bf85b5aa78dd1813156071
Jesus Reyes <luisdejesus200122@gmail.com>
2022-08-27 00:01:18 -0600

    Fixes #3: Hecho

---
+++ helpers/db-validators.js
@@ -0,0 +1,26 @@
+const Usuario = require('../models/usuario');
+
+const existeUsuarioPorId = async(id)=> {
+    const existeUsuairoPorId = await Usuario.findById(id);
+
+    if(!existeUsuairoPorId){
+         throw new Error(`El id no existe: ${id}`);
+    }  
+ 
+}
+
+const existeEmail = async (email)=> {
+    const existeEmail = await Usuario.findOne({email});
+
+    if(existeEmail){
+        throw new Error(`El Correo ${email} ya existe`);
+    }
+}
+
+
+module.exports  = {
+    existeUsuarioPorId,
+    existeEmail
+}
+
+ 
