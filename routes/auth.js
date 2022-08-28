b57215386b05d45f46af00854b83891f5689995b
Jesus Reyes <luisdejesus200122@gmail.com>
2022-08-27 22:47:15 -0600

    Fixes #10: Hecho

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { login } = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
 
 router.post('/',[
     check('email', 'El Email es obligatorio').isEmail(),
     check('password', 'El Password es obligatorio').not().isEmpty(),
+    validarCampos
 ],login);
 
 
